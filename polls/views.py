d3b7fcdaade1ece69b8e2605932ffc2d0e6eabfc
Matt Tomasini <matteo.tomasini@protonmail.com>
2022-12-12 14:54:14 +0100

    Updated views with forms

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -25,6 +26,17 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            "question":question,
+            "error_message": "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
     context = {"question": question}
     return render(request, 'polls/vote.html', context)
