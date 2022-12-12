842db84cde5b0baa366795af9d5de683bd7c7296
Matt Tomasini <matteo.tomasini@protonmail.com>
2022-12-12 12:10:02 +0100

    Wrote existing views as html templates

--- polls/views.py
+++ polls/views.py
@@ -12,11 +12,15 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    #return HttpResponse(f"You're looking at question {question_id}.")
+    context = {"question_id" : question_id}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    context = {"question_id" : question_id}
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    context = {"question_id" : question_id}
+    return render(request, 'polls/vote.html', context)
+    
