562db65b40f5528d332fa73e538de0f5041b8c37
Matt Tomasini <matteo.tomasini@protonmail.com>
2022-12-12 11:59:28 +0100

    Added views, following tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
